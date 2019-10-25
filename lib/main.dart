31ede1bf1877ee7f1fc6f0d82b18a52e2959913f
Victor Ruiz <vik_ases@live.com.mx>
2019-10-25 17:34:02 -0500

    Clase 61 Iniciando el proyecto componentes

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:curso_flutter_2/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Components App'),
+        ),
+        body: HomePageTemp()
+      ),
+    );
+  }
+}
