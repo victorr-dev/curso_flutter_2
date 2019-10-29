9bae495d4a7dd219aefb883472857b21c2d09899
Victor Ruiz <vik_ases@live.com.mx>
2019-10-29 03:10:09 -0600

    Clase 63 Mmetodo Map de las listas y ListTile

--- lib/pages/home_temp.dart
+++ lib/pages/home_temp.dart
@@ -11,26 +11,43 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: _crearOpciones()
+        children: _crearOpcionesB()
       ),
     );
   }
 
-  List<Widget> _crearOpciones(){
+  ////List<Widget> _crearOpciones(){
+////
+  ////  List<Widget> lista = List<Widget>();
+////
+  ////  for (String opt in _opciones) {
+  ////    
+  ////    final tempWidget = ListTile(
+  ////      title: Text(opt),
+  ////    );
+////
+  ////    lista..add(tempWidget)
+  ////          ..add(Divider());
+  ////  }
+////
+    //return lista;
 
-    List<Widget> lista = List<Widget>();
+  //}
 
-    for (String opt in _opciones) {
-      
-      final tempWidget = ListTile(
-        title: Text(opt),
-      );
-
-      lista..add(tempWidget)
-            ..add(Divider());
-    }
-
-    return lista;
-
+  List<Widget> _crearOpcionesB(){
+    return _opciones.map((opt) {
+        return Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(opt),
+              subtitle: Text("data"),
+              leading: Icon(Icons.access_alarm),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            Divider(),
+          ],
+        );
+    }).toList();
   }
 }
