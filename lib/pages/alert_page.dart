aed7d68edefd39b2deefe5f7175b2bb906f4a06b
Victor Ruiz <vik_ases@live.com.mx>
2019-11-04 23:08:07 -0600

    Clase 76: Alerts y Clase 77: Avatars

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -9,12 +9,51 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          child: Text('Activar Alerta'),
+          onPressed:() => _mostrarAlerta(context)
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.language),
         onPressed: (){
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Contenido de texto'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      }
     );
   }
 }
--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.language),
-        onPressed: (){
-          Navigator.pop(context);
-        },
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              radius: 25.0,
+              backgroundImage: NetworkImage('https://www.buscabiografias.com/img/people/Stanley_Kubrick.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
     );
   }
