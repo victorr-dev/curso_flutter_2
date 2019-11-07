a4b84ca7d88d64fe8d9ae8140b23099702cafcc1
Victor Ruiz <vik_ases@live.com.mx>
2019-11-06 22:00:32 -0600

    Clase 80: Tipos de inputs

--- lib/pages/fields_page.dart
+++ lib/pages/fields_page.dart
@@ -7,7 +7,8 @@ class FieldsPages extends StatefulWidget {
 
 class _FieldsPagesState extends State<FieldsPages> {
 
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +20,12 @@ class _FieldsPagesState extends State<FieldsPages> {
         children: <Widget>[
           _crearInput(), 
           Divider(),
+          _crearInputEmail(),
+          Divider(),
+          _crearInputPassword(),
+          Divider(),
           _crearPersona()
+
         ],
       ),
     );
@@ -49,10 +55,52 @@ class _FieldsPagesState extends State<FieldsPages> {
     );
   }
 
+  Widget _crearInputEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        ),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Email',
+        labelText: 'Email',
+        helperText: 'Solo es el Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email),
+      ),
+      onChanged: (e) {
+        setState(() => _email = e);
+      },
+    );
+  }
+
+  Widget _crearInputPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        helperText: 'Password',
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock_open),
+      ),
+      onChanged: (e) {
+        setState(() => _email = e);
+      },
+    );
+  }
+
+
   Widget _crearPersona() {
     return ListTile(
         title: Text('El nombre es: $_nombre'),
+        subtitle: Text('Email: $_email'),
     );
 
   }
+
 }
