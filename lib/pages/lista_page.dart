e52b462577e882ad586687ace9cbfa942126e5a3
Victor Ruiz <vik_ases@live.com.mx>
2019-11-14 14:22:54 -0600

    Clase 87: Implementacion del Infinite scroll

--- lib/pages/lista_page.dart
+++ lib/pages/lista_page.dart
@@ -8,7 +8,21 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _lista = [1,2,3,4,5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _lista = List();
+  int _ultimo = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener((){
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        _agregar10();
+      }
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -21,6 +35,7 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _lista.length,
       itemBuilder: (BuildContext context, int index){
         final imagen = _lista[index];
@@ -30,5 +45,16 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+  _agregar10(){
+    for (var i = 0; i < 10; i++) {
+      _ultimo++;
+      _lista.add(_ultimo);
+    }
+
+    setState(() {
+      
+    });
   }
 }
